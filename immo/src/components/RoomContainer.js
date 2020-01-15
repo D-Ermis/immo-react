421557ebbcff06e183853bb8298636b55004921a
Dogukan Ermis <ermisdogukan@gmail.com>
2020-01-15 16:39:31 +0100

    add RoomContainer containing filter and list component

---
+++ immo/src/components/RoomContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import RoomFilter from './RoomFilter';
+import RoomList from './RoomList';
+
+const RoomContainer = () => {
+  return (
+    <div>
+      hello from rooms container
+      <RoomFilter />
+      <RoomList />
+    </div>
+  );
+};
+
+export default RoomContainer;
