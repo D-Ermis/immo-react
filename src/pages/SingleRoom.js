3fa7abee24d635b83f092a6f5f5f65feea63ebe9
Dogukan Ermis <ermisdogukan@gmail.com>
2020-01-20 16:30:33 +0100

    remove empty strings

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -71,7 +71,7 @@ export default class SingleRoom extends Component {
               <h6>price : €{price}</h6>
               <h6>size : {size} SQFT</h6>
               <h6>
-                max capacity :{' '}
+                max capacity :
                 {capacity > 1 ? `${capacity} people` : `${capacity} person`}
               </h6>
               <h6>{pets ? 'pets allowed' : 'no pets allowed'}</h6>
