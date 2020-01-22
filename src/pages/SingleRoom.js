b5b6a1e50b084b855f9e127211955d6aed5aa507
Dogukan Ermis <ermisdogukan@gmail.com>
2020-01-22 16:36:19 +0100

    disable imgs for testing purposes

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -56,11 +56,11 @@ export default class SingleRoom extends Component {
           </Banner> */}
         {/* </StyledHero> */}
         <section className="single-room">
-          <div className="single-room-images">
+          {/* <div className="single-room-images">
             {defaultImg.map((item, index) => {
               return <img key={index} src={item} alt={name} />;
             })}
-          </div>
+          </div> */}
           {/* <div className="single-room-info">
             <article className="desc">
               <h3>details</h3>
