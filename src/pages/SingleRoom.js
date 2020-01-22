9b47140f9421d6d3ec57795d48036627a67e9a3c
Dogukan Ermis <ermisdogukan@gmail.com>
2020-01-22 16:23:21 +0100

    disable hero for testing purposes

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -48,13 +48,13 @@ export default class SingleRoom extends Component {
     const [mainImg, ...defaultImg] = images;
     return (
       <>
-        <StyledHero img={mainImg || this.state.defaultBcg}>
-          <Banner title={`${name} room`}>
+        {/* <StyledHero img={mainImg || this.state.defaultBcg}> */}
+        {/* <Banner title={`${name} room`}>
             <Link to="/houses" className="btn-primary">
               back to houses
             </Link>
-          </Banner>
-        </StyledHero>
+          </Banner> */}
+        {/* </StyledHero> */}
         <section className="single-room">
           <div className="single-room-images">
             {defaultImg.map((item, index) => {
